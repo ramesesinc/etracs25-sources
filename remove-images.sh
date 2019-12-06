1b588e369c4341b779ea21efe712c5f1791ad06e
worgflores <worgflores@gmail.com>
2019-12-06 17:42:56 +0800

    updated

--- remove-images.sh
+++ remove-images.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-docker rmi -f ramesesinc/etracs25-services:255.01.001
-docker rmi -f ramesesinc/etracs25-server-city:255.01.001
-docker rmi -f ramesesinc/etracs25-server-municipality:255.01.001
-docker rmi -f ramesesinc/etracs25-server-province:255.01.001
+docker rmi -f ramesesinc/etracs25-core:255.01
+docker rmi -f ramesesinc/etracs25-services:255.01.002
+docker rmi -f ramesesinc/etracs25-server-city:255.01.002
+docker rmi -f ramesesinc/etracs25-server-municipality:255.01.002
+docker rmi -f ramesesinc/etracs25-server-province:255.01.002
