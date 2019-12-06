e659a5700f023ac09082d60600d614746d15134b
worgflores <worgflores@gmail.com>
2019-12-06 17:48:16 +0800

    updated bash scripts

--- build-core.sh
+++ build-core.sh
@@ -2,20 +2,20 @@
 echo "[etracs25-core] build..."
 RUN_DIR=`pwd`
 cd $RUN_DIR/core/etracs25-core
-docker rmi -f ramesesinc/etracs25-core:255.01
-docker build -t ramesesinc/etracs25-core:255.01 .
+sudo docker rmi -f ramesesinc/etracs25-core:255.01
+sudo docker build -t ramesesinc/etracs25-core:255.01 .
 cd $RUN_DIR/core/etracs25-services
-docker rmi -f ramesesinc/etracs25-services:255.01.002
-docker build -t ramesesinc/etracs25-services:255.01.002 .
+sudo docker rmi -f ramesesinc/etracs25-services:255.01.002
+sudo docker build -t ramesesinc/etracs25-services:255.01.002 .
 cd $RUN_DIR/city
-docker rmi -f ramesesinc/etracs25-server-city:255.01.002
-docker build -t ramesesinc/etracs25-server-city:255.01.002 .
+sudo docker rmi -f ramesesinc/etracs25-server-city:255.01.002
+sudo docker build -t ramesesinc/etracs25-server-city:255.01.002 .
 cd $RUN_DIR/municipality
-docker rmi -f ramesesinc/etracs25-server-municipality:255.01.002
-docker build -t ramesesinc/etracs25-server-municipality:255.01.002 .
+sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.01.002
+sudo docker build -t ramesesinc/etracs25-server-municipality:255.01.002 .
 cd $RUN_DIR/province
-docker rmi -f ramesesinc/etracs25-server-province:255.01.002
-docker build -t ramesesinc/etracs25-server-province:255.01.002 .
+sudo docker rmi -f ramesesinc/etracs25-server-province:255.01.002
+sudo docker build -t ramesesinc/etracs25-server-province:255.01.002 .
 cd ..
 echo ""
 echo "[etracs25-core] finished."
--- build-services.sh
+++ build-services.sh
@@ -2,17 +2,17 @@
 echo "[etracs25-services] build..."
 RUN_DIR=`pwd`
 cd $RUN_DIR/core/etracs25-services
-docker rmi -f ramesesinc/etracs25-services:255.01.002
-docker build -t ramesesinc/etracs25-services:255.01.002 .
+sudo docker rmi -f ramesesinc/etracs25-services:255.01.002
+sudo docker build -t ramesesinc/etracs25-services:255.01.002 .
 cd $RUN_DIR/city
-docker rmi -f ramesesinc/etracs25-server-city:255.01.002
-docker build -t ramesesinc/etracs25-server-city:255.01.002 .
+sudo docker rmi -f ramesesinc/etracs25-server-city:255.01.002
+sudo docker build -t ramesesinc/etracs25-server-city:255.01.002 .
 cd $RUN_DIR/municipality
-docker rmi -f ramesesinc/etracs25-server-municipality:255.01.002
-docker build -t ramesesinc/etracs25-server-municipality:255.01.002 .
+sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.01.002
+sudo docker build -t ramesesinc/etracs25-server-municipality:255.01.002 .
 cd $RUN_DIR/province
-docker rmi -f ramesesinc/etracs25-server-province:255.01.002
-docker build -t ramesesinc/etracs25-server-province:255.01.002 .
+sudo docker rmi -f ramesesinc/etracs25-server-province:255.01.002
+sudo docker build -t ramesesinc/etracs25-server-province:255.01.002 .
 cd ..
 echo ""
 echo "[etracs25-services] finished."
