7ba9932d5e55e00c3a3add98bba41029ca4c3c05
worgflores <worgflores@gmail.com>
2019-12-26 13:44:15 +0800

    updated bash scripts

--- remove-images.sh
+++ remove-images.sh
@@ -4,15 +4,19 @@ sudo docker rmi -f ramesesinc/etracs25-core:255.01
 sudo docker rmi -f ramesesinc/etracs25-services:255.01.001
 sudo docker rmi -f ramesesinc/etracs25-services:255.01.002
 sudo docker rmi -f ramesesinc/etracs25-services:255.01.003
+sudo docker rmi -f ramesesinc/etracs25-services:255.01.004
 
 sudo docker rmi -f ramesesinc/etracs25-server-city:255.01.001
 sudo docker rmi -f ramesesinc/etracs25-server-city:255.01.002
 sudo docker rmi -f ramesesinc/etracs25-server-city:255.01.003
+sudo docker rmi -f ramesesinc/etracs25-server-city:255.01.004
 
 sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.01.001
 sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.01.002
 sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.01.003
+sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.01.004
 
 sudo docker rmi -f ramesesinc/etracs25-server-province:255.01.001
 sudo docker rmi -f ramesesinc/etracs25-server-province:255.01.002
 sudo docker rmi -f ramesesinc/etracs25-server-province:255.01.003
+sudo docker rmi -f ramesesinc/etracs25-server-province:255.01.004
