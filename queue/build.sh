7a21dc476387a2fb4d594884f427a3173cfb5133
worgflores <worgflores@gmail.com>
2019-12-28 10:28:25 +0800

    queue: updated

--- queue/build.sh
+++ queue/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-echo "[queue-server] build..."
+echo "[queue-server] removing previous build..."
+docker rmi -f ramesesinc/queue-server:255.01.001
 docker rmi -f ramesesinc/queue-server:255.01.002
+echo "[queue-server] build..."
 docker build -t ramesesinc/queue-server:255.01.002 .
 echo ""
 echo "[queue-server] finished."
