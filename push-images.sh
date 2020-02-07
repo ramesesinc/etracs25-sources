2cf2a7ce811e5584c089191490142aa9f5036569
worgflores <worgflores@gmail.com>
2020-02-07 13:32:30 +0800

    updated

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sudo docker push ramesesinc/etracs25-core:255.02
-sudo docker push ramesesinc/etracs25-services:255.02.002
-sudo docker push ramesesinc/etracs25-server-city:255.02.002
-sudo docker push ramesesinc/etracs25-server-municipality:255.02.002
-sudo docker push ramesesinc/etracs25-server-province:255.02.002
+sudo docker push ramesesinc/etracs25-server-city:255.02.003
+sudo docker push ramesesinc/etracs25-server-municipality:255.02.003
+sudo docker push ramesesinc/etracs25-server-province:255.02.003
--- remove-images.sh
+++ remove-images.sh
@@ -4,6 +4,7 @@ sudo docker rmi -f ramesesinc/etracs25-core:255.01
 
 sudo docker rmi -f ramesesinc/etracs25-services:255.02.001
 sudo docker rmi -f ramesesinc/etracs25-services:255.02.002
+sudo docker rmi -f ramesesinc/etracs25-services:255.02.003
 sudo docker rmi -f ramesesinc/etracs25-services:255.01.001
 sudo docker rmi -f ramesesinc/etracs25-services:255.01.002
 sudo docker rmi -f ramesesinc/etracs25-services:255.01.003
@@ -14,6 +15,7 @@ sudo docker rmi -f ramesesinc/etracs25-services:255.01.007
 
 sudo docker rmi -f ramesesinc/etracs25-server-city:255.02.001
 sudo docker rmi -f ramesesinc/etracs25-server-city:255.02.002
+sudo docker rmi -f ramesesinc/etracs25-server-city:255.02.003
 sudo docker rmi -f ramesesinc/etracs25-server-city:255.01.001
 sudo docker rmi -f ramesesinc/etracs25-server-city:255.01.002
 sudo docker rmi -f ramesesinc/etracs25-server-city:255.01.003
@@ -24,6 +26,7 @@ sudo docker rmi -f ramesesinc/etracs25-server-city:255.01.007
 
 sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.02.001
 sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.02.002
+sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.02.003
 sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.01.001
 sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.01.002
 sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.01.003
@@ -34,6 +37,7 @@ sudo docker rmi -f ramesesinc/etracs25-server-municipality:255.01.007
 
 sudo docker rmi -f ramesesinc/etracs25-server-province:255.02.001
 sudo docker rmi -f ramesesinc/etracs25-server-province:255.02.002
+sudo docker rmi -f ramesesinc/etracs25-server-province:255.02.003
 sudo docker rmi -f ramesesinc/etracs25-server-province:255.01.001
 sudo docker rmi -f ramesesinc/etracs25-server-province:255.01.002
 sudo docker rmi -f ramesesinc/etracs25-server-province:255.01.003
