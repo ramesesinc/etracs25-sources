fa62f2e0fd0ec4904917462380abbfa6900506d0
worgflores <worgflores@gmail.com>
2020-03-02 10:08:05 +0800

    updated to 255.02.006

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo docker push ramesesinc/etracs25-server-city:255.02.005
-sudo docker push ramesesinc/etracs25-server-municipality:255.02.005
-sudo docker push ramesesinc/etracs25-server-province:255.02.005
+sudo docker push ramesesinc/etracs25-server-city:255.02.006
+sudo docker push ramesesinc/etracs25-server-municipality:255.02.006
+sudo docker push ramesesinc/etracs25-server-province:255.02.006
