18f9875f9a895663131be822cbf23bc96d3d4a65
worgflores <worgflores@gmail.com>
2020-10-13 12:31:11 +0800

    updated

--- core/etracs-services/services/modules/treasury/rameses-treasury-common-services.jar/treasury/actions/AddCreditBillItem.groovy
+++ core/etracs-services/services/modules/treasury/rameses-treasury-common-services.jar/treasury/actions/AddCreditBillItem.groovy
@@ -9,25 +9,24 @@ import com.rameses.osiris3.common.*;
 
 /***
 * Description: Simple Add of Item. Item is unique based on the account. 
+* This is used for overpayment
 * Parameters:
 *    account 
 *    amount
 ****/
-class AddBillItem extends AbstractAddBillItem {
+class AddCreditBillItem extends AddBillItem {
 
 	public void execute(def params, def drools) {
 		def amt = params.amount.decimalValue;
-
-		int t = 0;
 
 		boolean hasAccount = ( params.account && params.account.key != "null" );
 		boolean hasTxntype = ( params.txntype && params.txntype.key != "null" );
 		boolean hasBillcode = ( params.billcode && params.billcode.key != "null" );
 
 		if(!hasAccount && !hasTxntype && !hasBillcode)
-			throw new Exception("AddBillItem error. Please specify an account, txntype or billcode in rule "  );
+			throw new Exception("AddCreditBillItem error. Please specify an account, txntype or billcode in rule "  );
 
-		def billitem = new BillItem(amount: NumberUtil.round( amt));
+		def billitem = new CreditBillItem(amount: NumberUtil.round( amt), txntype: 'credit');
 		if(  hasAccount ) {
 			setAccountFact( billitem, params.account.key );
 		}
@@ -37,15 +36,9 @@ class AddBillItem extends AbstractAddBillItem {
 		if( hasBillcode ) {
 			billitem.billcode = params.billcode.key;
 		}
-		//set the other parameters
-		if( params.year ) billitem.year = params.year.eval();	
-		if( params.month ) billitem.month = params.month.eval();		
-		if( params.fromdate ) billitem.fromdate = params.fromdate.eval();		
-		if( params.todate ) billitem.todate = params.todate.eval();		
-		if( params.remarks ) billitem.remarks = params.remarks.eval();
-		if( params.refid) billitem.refid = params.refid;
 
-		addToFacts( billitem );
+		getFacts().add( billitem );
 	}
+
 
 }
