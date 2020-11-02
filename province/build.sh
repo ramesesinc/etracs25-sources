516befda8acfd18b2ef859ed441aa01b935d3c0b
worgflores <worgflores@gmail.com>
2020-11-02 11:22:56 +0800

    province: 2.55.01.01

---
+++ province/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "[etracs-server-province] build..."
+docker rmi -f ramesesinc/etracs-server-province:2.55.01.01
+docker build -t ramesesinc/etracs-server-province:2.55.01.01 .
+echo ""
+echo "[etracs-server-province] finished."
