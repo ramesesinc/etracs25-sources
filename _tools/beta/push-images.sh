f77bee032fc6bb8dbbcb051ea459e383b958dca8
worgflores <worgflores@gmail.com>
2020-11-02 12:09:14 +0800

    added _tools

---
+++ _tools/beta/push-images.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+docker push ramesesinc/etracs-server-city:beta
+docker push ramesesinc/etracs-server-municipality:beta
+docker push ramesesinc/etracs-server-province:beta
---
+++ _tools/push-services.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+docker push ramesesinc/etracs-server-city:255.03.001
+docker push ramesesinc/etracs-server-municipality:255.03.001
+docker push ramesesinc/etracs-server-province:255.03.001
