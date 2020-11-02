fd70b1e7eee9bda84efb93fb5f57d1f99f73bd94
worgflores <worgflores@gmail.com>
2020-11-02 12:06:05 +0800

    removed

--- push-images.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo docker push ramesesinc/etracs25-server-city:255.02.007
-sudo docker push ramesesinc/etracs25-server-municipality:255.02.007
-sudo docker push ramesesinc/etracs25-server-province:255.02.007
