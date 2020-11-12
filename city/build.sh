eccaa0e6918c581f8f4b8baa21c4e333b3e39a05
worgflores <worgflores@gmail.com>
2020-11-12 21:15:01 +0800

    2.5.01.01

--- city/build.sh
+++ city/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[etracs-server-city] build..."
-docker rmi -f ramesesinc/etracs-server-city:2.55.01.01
-docker build -t ramesesinc/etracs-server-city:2.55.01.01 .
+docker rmi -f ramesesinc/etracs-server-city:2.5.01.01
+docker build -t ramesesinc/etracs-server-city:2.5.01.01 .
 echo ""
 echo "[etracs-server-city] finished."
--- core/etracs-core/build.sh
+++ core/etracs-core/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "[etracs-core] build..."
-docker rmi -f ramesesinc/etracs-core:2.55
-docker build -t ramesesinc/etracs-core:2.55 .
+docker rmi -f ramesesinc/etracs-core:2.5.01
+docker build -t ramesesinc/etracs-core:2.5.01 .
 echo "[etracs-core] finished."
