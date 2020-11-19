723ced2c98f5005a1dd7f9529255c36c88c79186
worgflores <worgflores@gmail.com>
2020-11-19 15:20:45 +0800

    notification-ws: updated

--- core/notification-ws/build.sh
+++ core/notification-ws/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[notification-ws] build..."
-docker rmi -f ramesesinc/notification-ws:2.55
-docker build -t ramesesinc/notification-ws:2.55 .
+docker rmi -f ramesesinc/notification-ws:2.5.01
+docker build -t ramesesinc/notification-ws:2.5.01 .
 echo ""
 echo "[notification-ws] finished."
