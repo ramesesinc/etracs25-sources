67a2b301d18a0ec0d96456c606184da12d8fcdd0
worgflores <worgflores@gmail.com>
2020-12-29 00:42:24 +0800

    upgraded

--- province/build.sh
+++ province/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[etracs-server-province] build..."
-docker rmi -f ramesesinc/etracs-server-province:2.5.02.02.01
-docker build -t ramesesinc/etracs-server-province:2.5.02.02.01 .
+docker rmi -f ramesesinc/etracs-server-province:2.5.02.03.01
+docker build -t ramesesinc/etracs-server-province:2.5.02.03.01 .
 echo ""
 echo "[etracs-server-province] finished."
