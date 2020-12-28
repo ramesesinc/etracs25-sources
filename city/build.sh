955db505220b9699f98becf07f7523bc3ab4efed
Jessie S. Zamora <jzamss@gmail.com>
2020-12-28 23:15:15 +0800

    build: bump version

--- city/build.sh
+++ city/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[etracs-server-city] build..."
-docker rmi -f ramesesinc/etracs-server-city:2.5.02.02.01
-docker build -t ramesesinc/etracs-server-city:2.5.02.02.01 .
+docker rmi -f ramesesinc/etracs-server-city:2.5.02.03.01
+docker build -t ramesesinc/etracs-server-city:2.5.02.03.01 .
 echo ""
 echo "[etracs-server-city] finished."
