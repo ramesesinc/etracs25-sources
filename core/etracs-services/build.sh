5089e7ff696801296f7ce28ed175798910a2fea3
worgflores <worgflores@gmail.com>
2021-01-14 16:15:06 +0800

    upgraded 2.5.02.06.01

--- core/etracs-services/build.sh
+++ core/etracs-services/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[etracs-services] build..."
-docker rmi -f ramesesinc/etracs-services:2.5.02.05
-docker build -t ramesesinc/etracs-services:2.5.02.05 .
+docker rmi -f ramesesinc/etracs-services:2.5.02.06
+docker build -t ramesesinc/etracs-services:2.5.02.06 .
 echo ""
 echo "[etracs-services] finished."
