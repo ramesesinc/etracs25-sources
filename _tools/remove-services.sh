12bb54911800b4351f08d1a1209fae8874104276
worgflores <worgflores@gmail.com>
2021-02-18 13:17:23 +0800

    etracs-server: 2.5.03.02

--- _tools/remove-services.sh
+++ _tools/remove-services.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker rmi -f ramesesinc/etracs-server-city:2.5.03.01.01
-docker rmi -f ramesesinc/etracs-server-province:2.5.03.01.01
-docker rmi -f ramesesinc/etracs-server-municipality:2.5.03.01.01
-docker rmi -f ramesesinc/etracs-services:2.5.03.01
+docker rmi -f ramesesinc/etracs-server-city:2.5.03.02.01
+docker rmi -f ramesesinc/etracs-server-province:2.5.03.02.01
+docker rmi -f ramesesinc/etracs-server-municipality:2.5.03.02.01
+docker rmi -f ramesesinc/etracs-services:2.5.03.02
--- core/etracs-services/build.sh
+++ core/etracs-services/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[etracs-services] build..."
-docker rmi -f ramesesinc/etracs-services:2.5.03.01
-docker build -t ramesesinc/etracs-services:2.5.03.01 .
+docker rmi -f ramesesinc/etracs-services:2.5.03.02
+docker build -t ramesesinc/etracs-services:2.5.03.02 .
 echo ""
 echo "[etracs-services] finished."
