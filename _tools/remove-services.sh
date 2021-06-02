f0797248c669045dcd62dc17bc84955b92c62ebb
worgflores <worgflores@gmail.com>
2021-06-02 14:06:54 +0800

    _tools: updated

--- _tools/remove-services.sh
+++ _tools/remove-services.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker rmi -f ramesesinc/etracs-server-city:2.5.03.02.01
-docker rmi -f ramesesinc/etracs-server-province:2.5.03.02.01
-docker rmi -f ramesesinc/etracs-server-municipality:2.5.03.02.01
-docker rmi -f ramesesinc/etracs-services:2.5.03.02
+docker rmi -f ramesesinc/etracs-server-city:2.5.04.01.01
+docker rmi -f ramesesinc/etracs-server-province:2.5.04.01.01
+docker rmi -f ramesesinc/etracs-server-municipality:2.5.04.01.01
+docker rmi -f ramesesinc/etracs-services:2.5.04.01
