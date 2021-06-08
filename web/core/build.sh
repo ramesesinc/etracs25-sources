ac065b8a0b6f08ef96671cf9f774ca1630b2205a
worgflores <worgflores@gmail.com>
2021-06-08 10:00:08 +0800

    web upgraded to 2.5.02

--- web/core/build.sh
+++ web/core/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[web-core] build..."
-docker rmi -f ramesesinc/etracs-web-core:2.5.01
-docker build -t ramesesinc/etracs-web-core:2.5.01 .
+docker rmi -f ramesesinc/etracs-web-core:2.5.02
+docker build -t ramesesinc/etracs-web-core:2.5.02 .
 echo ""
 echo "[web-core] finished."
--- web/etracs/build.sh
+++ web/etracs/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[etracs-web] build..."
-docker rmi -f ramesesinc/etracs-web:2.5.01.01
-docker build -t ramesesinc/etracs-web:2.5.01.01 .
+docker rmi -f ramesesinc/etracs-web:2.5.02.01
+docker build -t ramesesinc/etracs-web:2.5.02.01 .
 echo ""
 echo "[etracs-web] finished."
